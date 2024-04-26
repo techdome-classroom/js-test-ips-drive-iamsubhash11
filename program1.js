7cf1aa371aaf52fc7760f1bbec5de7faebf0ec3d
iamsubhash11 <168169367+iamsubhash11@users.noreply.github.com>
2024-04-26 13:26:57 +0530

    Update program1.js

--- program1.js
+++ program1.js
@@ -1,9 +1,31 @@
-function smallestMissingPositiveInteger(nums) {
-
-
-    // Implement the function smallest_missing_positive_integer
-  
+ 
+    function smallestMissingPositiveInteger(nums) {
+      const n = nums.length;
+      
+       let j = 0;
+      for (let i = 0; i < n; i++) {
+          if (nums[i] <= 0) {
+              [nums[i], nums[j]] = [nums[j], nums[i]];
+              j++;
+          }
+      }
+      
+       const positiveNums = nums.slice(j);
+      
+       for (let i = 0; i < positiveNums.length; i++) {
+          const index = Math.abs(positiveNums[i]) - 1;
+          if (index < positiveNums.length && positiveNums[index] > 0) {
+              positiveNums[index] = -positiveNums[index];
+          }
+      }
+      
+       for (let i = 0; i < positiveNums.length; i++) {
+          if (positiveNums[i] > 0) {
+              return i + 1;
+          }
+      }
+      
+       return positiveNums.length + 1;
   }
-  
+    
   module.exports = smallestMissingPositiveInteger;
-
