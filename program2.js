0cedc15a7030445cca78ce5edcc59db26cf80202
iamsubhash11 <168169367+iamsubhash11@users.noreply.github.com>
2024-04-26 13:27:18 +0530

    Update program2.js

--- program2.js
+++ program2.js
@@ -1,7 +1,24 @@
-function longestSubstring(s) {
 
+    function longestSubstring(s) {
+        let maxLength = 0;
+        let left = 0;
+        const charIndexMap = new Map();  
+    
+        for (let right = 0; right < s.length; right++) {
+            const currentChar = s[right];
+    
+             
+            if (charIndexMap.has(currentChar) && charIndexMap.get(currentChar) >= left) {
+                left = charIndexMap.get(currentChar) + 1;
+            }
+    
+             charIndexMap.set(currentChar, right);
+    
+             maxLength = Math.max(maxLength, right - left + 1);
+        }
+    
+        return maxLength;
+    }
 
-    // Implementation of longestSubstring function
-}
 
 module.exports = { longestSubstring };
